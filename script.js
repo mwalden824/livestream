68ecfee20cc162e6de0a1eda7b0ca0b56a63e895
Mr. Walden <mwalden824@gmail.com>
2023-07-18 19:30:37 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// const sidebarToggle = document.getElementById("sidebar-toggle");
+// const sidebar = document.querySelector(".sidebar");
+// const content = document.querySelector(".content");
+
+// sidebarToggle.addEventListener("click", function () {
+//   sidebar.classList.toggle("open");
+//   content.classList.toggle("open");
+// });
+
+function show() {
+    document.getElementById('sidebar').classList.toggle('active');
+    console.log("Test");
+}
