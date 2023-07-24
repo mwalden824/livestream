69d64e7c3ce6fb9334b5949669de4a7f792ab246
Mr. Walden <mwalden824@gmail.com>
2023-07-23 22:27:35 -0500

    Finished static layout except for profile area.  Just text for now

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var chatOpen = true;
+const chatButton = document.getElementById('open-chat-button');
+const offlineChatButton = document.getElementById('open-chat-button-offline');
+
 function show() {
     document.getElementById('sidebar').classList.toggle('active');
     document.getElementById('main').classList.toggle('active');
@@ -5,6 +9,24 @@ function show() {
 }
 
 function showChat() {
+    if (chatOpen == true) {
+        chatOpen = false;
+        if (isPlaying) {
+            chatButton.style.display = 'block';
+        }
+        else {
+            offlineChatButton.style.display = 'block';
+        }
+    } else {
+        chatOpen = true;
+        if (isPlaying) {
+            chatButton.style.display = 'none';
+        }
+        else {
+            offlineChatButton.style.display = 'none';
+        }
+    }
+
     document.getElementById('left-sidebar').classList.toggle('active');
     document.getElementById('main').classList.toggle('left-active');
 }
