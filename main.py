416a5ffaa75c17256bccd4a722e9445f87b5bfc2
Mr. Walden <mwalden824@gmail.com>
2023-08-11 12:16:00 -0500

    Added stuff

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 from flask import Flask, render_template, request, session, redirect, url_for, send_file
 from flask_socketio import join_room, leave_room, send, SocketIO
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 import random
 from string import ascii_letters
+from werkzeug.security import generate_password_hash, check_password_hash
+from os import path
+
+DB_NAME = "database.db"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysupersecretkeybitch"
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+db = SQLAlchemy(app)
+# db.init_app(app)
+
+
+# class User(db.Model, UserMixin):
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+
+
+if not path.exists(DB_NAME):
+    with app.app_context():
+        db.create_all()
+        # db.create_all(app=app)
+        print("Database created")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 socketio = SocketIO(app)
 
 # Set this to streamer's username when you setup account, auth, etc.
@@ -20,15 +54,89 @@ def generate_random_username():
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    session.clear()
-    session["name"] = generate_random_username()
-    if session.get("name") is None:
-        # Handle case where user hasn't set their name
-        return    
-
-    print("The messages are: ")
-    print(messages)
+    # session.clear()
+    # session["name"] = generate_random_username()
+    # if session.get("name") is None:
+    #     # Handle case where user hasn't set their name
+    #     return
+    
+    # print("The messages are: ")
+    # print(messages)
     return render_template("index.html", msgs=messages)
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username-login')
+        password = request.form.get('password-login')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                # flash('')
+                print("Logged in Successfully")
+                login_user(user, remember=True)
+                # return redirect(url_for('home'))
+                # print("User: " + str(user.username))
+                session["name"] = str(user.username)
+                # return 'Login Successful'
+                return redirect(request.referrer)
+            else:
+                # flash('')
+                print("Incorrect Password")
+        else:
+            print("Username doesn't exist")
+            # flash('')
+
+    # return render_template("index.html", msgs=messages)
+    return 'Authentication Error'
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username-signup')
+        email = request.form.get('email-signup')
+        password = request.form.get('password-signup')
+        password2 = request.form.get('password2-signup')
+
+    print('TESTING 123')
+    user = User.query.filter_by(email=email).first()
+    if user:
+        # flash('Email already exists.')
+        print('Email already exists')
+    elif len(email) < 4:
+        # flash('')
+        print('Email too short')
+    elif len(username) < 4:
+        # flash('')
+        print('Username too short')
+    elif password != password2:
+        # flash('')
+        print("Passwords don't match")
+    elif len(password) < 8:
+        # flash('')
+        print('password too short')
+    else:
+        new_user = User(username=username, email=email, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        # flash('')
+        print('Account created')
+        # return 'Account created'
+        # return '<script>loginSuccessful();</script>'
+        return redirect(request.referrer)
+        # return redirect(url_for(home))
+    
+    # return render_template('index.html', msgs=messages)
+    return 'Error in Account Creation'
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    # TODO: reload the current page
+    # return redirect(url_for(home))
+    return redirect(request.referrer)
 
 @app.route('/stream/hls/stream.m3u8')
 def serve_playlist():
@@ -52,7 +160,7 @@ def message(data):
     # send(content)
     messages.append(content)
     print(f"{session.get('name')} said: {data['data']}")
-    print(messages)
+    # print(messages)
 
 @socketio.on("connect")
 def connect(auth):
@@ -77,5 +185,5 @@ def disconnect():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5001)
 
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,6 +2,40 @@ var chatOpen = true;
 const chatButton = document.getElementById('open-chat-button');
 const offlineChatButton = document.getElementById('open-chat-button-offline');
 
+var imageFallback = document.getElementById('offline-image-placeholder');
+var videoElement = document.getElementById('videoElement');
+var videoPlayerElement = document.getElementById('videoPlayer');
+var openChatBtn = document.getElementById('open-chat-btn');
+var openChatBtnOffline = document.getElementById('open-chat-button-offline');
+
+var isPlaying = true;
+
+openChatBtnOffline.style.display = 'none';
+videoPlayerElement.addEventListener("error", function() {
+    console.log("Video Error");
+    videoElement.style.display = 'none';
+    imageFallback.style.display = 'block';
+    isPlaying = false;
+});
+var videoPlayer = videojs('videoPlayer', {
+    autoplay: 'muted',
+    muted: true
+});
+videoPlayer.src({
+    // src: '../stream/hls/stream.m3u8',
+    src: '/stream/hls/stream.m3u8',
+    // src: '/home/mwalden/Projects/livestream/stream/hls/stream.m3u8',
+    type: 'application/x-mpegURL'
+});
+videoPlayer.on('error', function (event) {
+    console.log('Video Error');
+    videoElement.style.display = 'none';
+    imageFallback.style.display = 'block';
+    isPlaying = false;
+});
+videoPlayer.play();
+videoPlayer.muted(false);        
+
 function show() {
     document.getElementById('sidebar').classList.toggle('active');
     document.getElementById('main').classList.toggle('active');
@@ -31,10 +65,10 @@ function showChat() {
     document.getElementById('main').classList.toggle('left-active');
 }
 
-
 var socketio = io();
 
 const messages = document.getElementById('chatMessages');
+// messages.scrollTop = messages.scrollHeight;
 
 const createMessage = (name, msg) => {
     const msgContent = `
@@ -49,6 +83,7 @@ const createMessage = (name, msg) => {
 socketio.on("message", (data) => {
     console.log("A message has been transmitted");
     createMessage(data.name, data.message);
+    messages.scrollTop = messages.scrollHeight;
 });
 
 const sendMessage = () => {
@@ -65,3 +100,24 @@ msgTextBox.addEventListener("keydown", function (e) {
     }
 });
 
+function showSignupPopup() {
+    document.getElementById('signupPopup').style.display = 'block';
+}
+
+function closeSignupPopup() {
+    document.getElementById('signupPopup').style.display = 'none';
+}
+
+function showLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'block';
+}
+
+function closeLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'none';
+}
+
+// Add a click event listener to the signup button
+document.getElementById('signup-button').addEventListener('click', showSignupPopup);
+
+// Add a click event listener to the login button
+document.getElementById('login-button').addEventListener('click', showLoginPopup);
